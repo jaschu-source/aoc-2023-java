feccf26911cceaca65417e4c8133a2642481e151
jaschu-source <jas.schulze@gmail.com>
2024-09-02 18:43:22 +0200

    D03-1

--- src/main/java/org/jaschu/christmas/fun/day03/Day03.java
+++ src/main/java/org/jaschu/christmas/fun/day03/Day03.java
@@ -2,6 +2,8 @@ package org.jaschu.christmas.fun.day03;
 
 import org.jaschu.christmas.fun.common.AbstractPuzzle;
 
+import java.util.List;
+
 public class Day03 extends AbstractPuzzle {
     /**
      * Constructor which accepts the puzzle input to be solved.
@@ -14,7 +16,30 @@ public class Day03 extends AbstractPuzzle {
 
     @Override
     public String solvePart1() {
-        return null;
+        List<String> lines = this.readFileLines();
+        int schematicsSum = 0;
+        // collect some additional info first
+        List<List<SchematicsEntry>> schematicsEntries = lines.stream()
+                .map((line) -> line.chars().mapToObj(c -> new SchematicsEntry((char) c)).toList())
+                .toList();
+
+        for(int rowNumber = 0; rowNumber < schematicsEntries.size(); rowNumber++) {
+            for(int position = 0; position < schematicsEntries.get(rowNumber).size(); position++) {
+                SchematicsEntry schematicsEntry = schematicsEntries.get(rowNumber).get(position);
+                if(schematicsEntry.getSchematicsType().equals(SchematicsType.NUMBER) && !schematicsEntry.isAlreadyCounted()) {
+                    schematicsEntry.setAlreadyCounted(true);
+                    schematicsEntry.setFullNumber(schematicsEntries.get(rowNumber), position);
+
+                    if (schematicsEntry.isSchematic(rowNumber, position, schematicsEntries)) {
+                        schematicsSum += Integer.parseInt(schematicsEntry.getFullNumberWithSurroundings());
+                    }
+
+
+                }
+            }
+        }
+
+        return String.valueOf(schematicsSum);
     }
 
     @Override
--- src/main/java/org/jaschu/christmas/fun/day03/SchematicsEntry.java
+++ src/main/java/org/jaschu/christmas/fun/day03/SchematicsEntry.java
@@ -1,15 +1,79 @@
 package org.jaschu.christmas.fun.day03;
 
-import lombok.Builder;
 import lombok.Data;
 
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Data
-@Builder
 public class SchematicsEntry {
     private Character characterValue;
     private SchematicsType schematicsType;
 
     // only necessary if schematicsType is NUMBER
     private boolean alreadyCounted = false;
-    private int numericValue = 0;
+    private String fullNumberWithSurroundings;
+
+    public SchematicsEntry(Character characterValue) {
+        this.characterValue = characterValue;
+        this.fullNumberWithSurroundings = String.valueOf(characterValue);
+        Pattern numberPattern = Pattern.compile("\\d");
+        Matcher numberMatcher = numberPattern.matcher(characterValue.toString());
+        if(characterValue.equals('.')) {
+            this.schematicsType = SchematicsType.DOT;
+        } else if(numberMatcher.matches()) {
+            this.schematicsType = SchematicsType.NUMBER;
+        } else {
+            this.schematicsType = SchematicsType.SYMBOL;
+        }
+    }
+
+    /**
+     *
+     * @param schematicsEntries: list to search for full number
+     * @param startPosition: position the number starts on
+     */
+    public void setFullNumber(List<SchematicsEntry> schematicsEntries, int startPosition) {
+        searchFullNumber(schematicsEntries, this, startPosition);
+    }
+
+    /**
+     *
+     * @param schematicsEntries: list to search for full number
+     * @param entry: position the number starts on
+     * @param startPosition: position the number starts on
+     */
+    private static void searchFullNumber(List<SchematicsEntry> schematicsEntries, SchematicsEntry entry, int startPosition) {
+        int entryPosition = startPosition + 1;
+        if(startPosition < schematicsEntries.size() && entryPosition < schematicsEntries.size()) {
+            SchematicsEntry followingEntry = schematicsEntries.get(entryPosition);
+            if (followingEntry.getSchematicsType().equals(SchematicsType.NUMBER)) {
+                entry.fullNumberWithSurroundings = entry.getFullNumberWithSurroundings() + followingEntry.getCharacterValue();
+                followingEntry.setAlreadyCounted(true);
+                searchFullNumber(schematicsEntries, entry, startPosition + 1);
+            }
+        }
+    }
+
+    public boolean isSchematic(int rowNumber, int schemaPosition, List<List<SchematicsEntry>> schematicsEntries) {
+        //determine surrounding positions
+        int numberEnd = schemaPosition + this.getFullNumberWithSurroundings().length() - 1;
+
+        boolean schematic = false;
+
+        for(int currentRow = rowNumber - 1; currentRow <= rowNumber +1 ; currentRow++) {
+            for(int position = schemaPosition - 1; position <= numberEnd + 1; position++) {
+                if(schematicsEntries.size() > currentRow
+                        && currentRow >= 0
+                        && position >= 0
+                        && schematicsEntries.get(currentRow).size() > position
+                        && schematicsEntries.get(currentRow).get(position).schematicsType.equals(SchematicsType.SYMBOL)) {
+                    schematic = true;
+                    break;
+                }
+            }
+        }
+        return schematic;
+    }
 }
