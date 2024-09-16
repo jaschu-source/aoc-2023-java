0890e04de86bff648da7e11f65b3fb3c595793ac
jaschu-source <jas.schulze@gmail.com>
2024-09-16 08:00:23 +0200

    D05-1: preparation

--- src/main/java/org/jaschu/christmas/fun/day05/Almanac.java
+++ src/main/java/org/jaschu/christmas/fun/day05/Almanac.java
@@ -2,21 +2,53 @@ package org.jaschu.christmas.fun.day05;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Data
 public class Almanac {
     private List<Integer> seeds;
-    private Map<Integer, Integer> SeedToSoilMap;
-    private Map<Integer, Integer> SoilToFertilizerMap;
-    private Map<Integer, Integer> FertilizerToWaterMap;
-    private Map<Integer, Integer> WaterToLightMap;
-    private Map<Integer, Integer> LightToTemperatureMap;
-    private Map<Integer, Integer> TemperatureToHumidityMap;
-    private Map<Integer, Integer> HumidityToLocationMap;
+    private Map<Integer, Integer> seedToSoilMap;
+    private Map<Integer, Integer> soilToFertilizerMap;
+    private Map<Integer, Integer> fertilizerToWaterMap;
+    private Map<Integer, Integer> waterToLightMap;
+    private Map<Integer, Integer> lightToTemperatureMap;
+    private Map<Integer, Integer> temperatureToHumidityMap;
+    private Map<Integer, Integer> humidityToLocationMap;
+
+    private int initialisationIndex = 0; // only needed for centralized initialization process in constructor
 
     public Almanac(List<String> input) {
+        while (initialisationIndex < input.size()) {
+            String[] almanacIdentifier = input.get(initialisationIndex).split(":");
+            switch (almanacIdentifier[0]) {
+                case AlmanacInfo.SEEDS -> setSeeds(almanacIdentifier[1]);
+                case AlmanacInfo.SEED_TO_SOIL -> this.seedToSoilMap = generateAlmanacMapFromStringInput(input);
+                case AlmanacInfo.SOIL_TO_FERTILIZER ->
+                        this.soilToFertilizerMap = generateAlmanacMapFromStringInput(input);
+                case AlmanacInfo.FERTILIZER_TO_WATER ->
+                        this.fertilizerToWaterMap = generateAlmanacMapFromStringInput(input);
+                case AlmanacInfo.WATER_TO_LIGHT -> this.waterToLightMap = generateAlmanacMapFromStringInput(input);
+                case AlmanacInfo.LIGHT_TO_TEMPERATURE ->
+                        this.lightToTemperatureMap = generateAlmanacMapFromStringInput(input);
+                case AlmanacInfo.TEMPERATURE_TO_HUMIDITY ->
+                        this.temperatureToHumidityMap = generateAlmanacMapFromStringInput(input);
+                case AlmanacInfo.HUMIDITY_TO_LOCATION ->
+                        this.humidityToLocationMap = generateAlmanacMapFromStringInput(input);
+                default -> initialisationIndex++;
+            }
+        }
+    }
 
+    public void setSeeds(String seeds) {
+        this.seeds = new ArrayList<>();
+        initialisationIndex++;
+    }
+
+    private Map<Integer, Integer> generateAlmanacMapFromStringInput(List<String> input) {
+        initialisationIndex++;
+        return new HashMap<>();
     }
 }
--- src/main/java/org/jaschu/christmas/fun/day05/AlmanacInfo.java
+++ src/main/java/org/jaschu/christmas/fun/day05/AlmanacInfo.java
@@ -1,18 +1,12 @@
 package org.jaschu.christmas.fun.day05;
 
-public enum AlmanacInfo {
-    SEEDS("seeds: "),
-    SEED_TO_SOIL("seed-to-soil map:"),
-    SOIL_TO_FERTILIZER("soil-to-fertilizer map:"),
-    FERTILIZER_TO_WATER("fertilizer-to-water map:"),
-    WATER_TO_LIGHT("water-to-light map:"),
-    LIGHT_TO_TEMPERATURE("light-to-temperature map:"),
-    TEMPERATURE_TO_HUMIDITY("temperature-to-humidity map:"),
-    HUMIDITY_TO_LOCATION("humidity-to-location map:");
-
-    public final String label;
-
-    AlmanacInfo(String label) {
-        this.label = label;
-    }
+public class AlmanacInfo {
+    public static final String SEEDS = "seeds";
+    public static final String SEED_TO_SOIL = "seed-to-soil map";
+    public static final String SOIL_TO_FERTILIZER = "soil-to-fertilizer map";
+    public static final String FERTILIZER_TO_WATER = "fertilizer-to-water map";
+    public static final String WATER_TO_LIGHT = "water-to-light map";
+    public static final String LIGHT_TO_TEMPERATURE = "light-to-temperature map";
+    public static final String TEMPERATURE_TO_HUMIDITY = "temperature-to-humidity map";
+    public static final String HUMIDITY_TO_LOCATION = "humidity-to-location map";
 }
